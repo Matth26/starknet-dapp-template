b5550defe1907326326c2a6da94ad7b23f1fa010
Matthias Monnier <matthias.monnier@gmail.com>
2022-09-15 16:01:09 +0200

    Add table with txs

--- src/components/YourApp.tsx
+++ src/components/YourApp.tsx
@@ -1,4 +1,8 @@
-import { useStarknet, useStarknetInvoke } from '@starknet-react/core';
+import {
+  useStarknet,
+  useStarknetCall,
+  useStarknetInvoke,
+} from '@starknet-react/core';
 import { useContract } from '@starknet-react/core';
 import { hexToAscii } from 'web3-utils';
 
@@ -12,6 +16,7 @@ import { BigNumberish, toFelt } from 'starknet/dist/utils/number';
 import BN from 'bn.js';
 import { useState } from 'react';
 import { Group, NumberInput } from '@mantine/core';
+import TxTable from './TxTable';
 const CONTRACT_ADDRESS =
   '0x058c546adb10ef6b7827b02e45ecac33a54cc0c87e80f245090d0aaae5cbe2d9';
 
@@ -32,42 +37,63 @@ const YourApp = () => {
   const [wings, setWings] = useState(0);
 
   const {
-    data,
+    /*data,
     loading,
     error,
-    reset,
+    reset,*/
     invoke: invokeDeclareAnimal,
   } = useStarknetInvoke({
     contract,
     method: 'declare_animal',
   });
 
+  const {
+    data: name2,
+    loading: isNameLoading,
+    error: nameError,
+    refresh: refreshName,
+  } = useStarknetCall({
+    contract,
+    method: 'name',
+    args: [],
+    options: { watch: false },
+  });
+  console.log(name2);
+  console.log(isNameLoading);
+  console.log(nameError);
+
   return (
     <div>
       <Connectors />
       <div>gm {account}</div>
 
-      {account && (
+      {account && contract && (
         <>
-          <Group position="center">
-            {contract ? (
-              <button
-                onClick={async () => {
-                  const res = await contract.name();
-                  //console.log(feltToString(res.name));
-                  setName(hexToAscii('0x' + res.name.toString(16)));
-                }}
-              >
-                Get View
-              </button>
-            ) : (
-              <div>Not Connected</div>
-            )}
+          <Group position="center" mt={5}>
+            <button
+              onClick={async () => {
+                const res = await contract.name();
+                //console.log(feltToString(res.name));
+                setName(hexToAscii('0x' + res.name.toString(16)));
+              }}
+            >
+              Get View from starknet.js
+            </button>
             <span>Name</span>
             <span>{name !== '' && name}</span>
           </Group>
 
-          <Group>
+          <Group position="center" mt={5}>
+            <button onClick={() => refreshName()}>
+              Get View from Starknet React
+            </button>
+            <span>Name</span>
+            <span>
+              {name2 && name2[0] && hexToAscii('0x' + name2[0].toString(16))}
+            </span>
+          </Group>
+
+          <Group mt={5}>
             <NumberInput
               placeholder="1"
               label="Sex"
@@ -93,7 +119,7 @@ const YourApp = () => {
               <button
                 onClick={async () => {
                   invokeDeclareAnimal({
-                    args: [toFelt(sex), toFelt(legs), toFelt(wings)],
+                    args: [sex, toFelt(legs), toFelt(wings)],
                     overrides: { maxFee: 100000 },
                   });
                 }}
@@ -101,8 +127,8 @@ const YourApp = () => {
                 Declare Animal
               </button>
             )}
-            <Group>{loading ? <div>loading...</div> : <div>{data}</div>}</Group>
           </Group>
+          <TxTable />
         </>
       )}
     </div>
