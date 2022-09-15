ed495f731e9cf6a3850b4146c7edc4c34feddc3b
Matthias Monnier <matthias.monnier@gmail.com>
2022-09-15 15:31:19 +0200

    Invoke function works

--- src/components/YourApp.tsx
+++ src/components/YourApp.tsx
@@ -1,15 +1,19 @@
-import { useStarknet } from '@starknet-react/core';
+import { useStarknet, useStarknetInvoke } from '@starknet-react/core';
 import { useContract } from '@starknet-react/core';
 import { hexToAscii } from 'web3-utils';
 
 import Connectors from './Connectors';
 
-import ContractAbi from '../assets/abis/ERC721_Metadata.json';
+import { useStarknetExecute } from '@starknet-react/core';
+
+import ContractAbi from '../assets/abis/ERC721.json';
 import { Abi } from 'starknet';
+import { BigNumberish, toFelt } from 'starknet/dist/utils/number';
 import BN from 'bn.js';
-import { Suspense, useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Group, NumberInput } from '@mantine/core';
 const CONTRACT_ADDRESS =
-  '0x04222f5f5d1e03657e505485a20aa12952c1c3b79614551000bd8aa8f4c32f5a';
+  '0x058c546adb10ef6b7827b02e45ecac33a54cc0c87e80f245090d0aaae5cbe2d9';
 
 export function feltToString(felt: BN) {
   const newStrB = Buffer.from(felt.toString(16), 'hex');
@@ -23,28 +27,84 @@ const YourApp = () => {
     address: CONTRACT_ADDRESS,
   });
   const [name, setName] = useState('');
+  const [sex, setSex] = useState(0);
+  const [legs, setLegs] = useState(0);
+  const [wings, setWings] = useState(0);
+
+  const {
+    data,
+    loading,
+    error,
+    reset,
+    invoke: invokeDeclareAnimal,
+  } = useStarknetInvoke({
+    contract,
+    method: 'declare_animal',
+  });
 
   return (
     <div>
-      <Connectors></Connectors>
+      <Connectors />
       <div>gm {account}</div>
-      {contract ? (
-        <button
-          onClick={async () => {
-            const res = await contract.name();
-            //console.log(feltToString(res.name));
-            setName(hexToAscii('0x' + res.name.toString(16)));
-          }}
-        >
-          Get View
-        </button>
-      ) : (
-        <div>Not Connected</div>
+
+      {account && (
+        <>
+          <Group position="center">
+            {contract ? (
+              <button
+                onClick={async () => {
+                  const res = await contract.name();
+                  //console.log(feltToString(res.name));
+                  setName(hexToAscii('0x' + res.name.toString(16)));
+                }}
+              >
+                Get View
+              </button>
+            ) : (
+              <div>Not Connected</div>
+            )}
+            <span>Name</span>
+            <span>{name !== '' && name}</span>
+          </Group>
+
+          <Group>
+            <NumberInput
+              placeholder="1"
+              label="Sex"
+              withAsterisk
+              value={sex}
+              onChange={(val) => val && setSex(val)}
+            />
+            <NumberInput
+              placeholder="1"
+              label="Legs"
+              withAsterisk
+              value={legs}
+              onChange={(val) => val && setLegs(val)}
+            />
+            <NumberInput
+              placeholder="1"
+              label="Wings"
+              withAsterisk
+              value={wings}
+              onChange={(val) => val && setWings(val)}
+            />
+            {contract && (
+              <button
+                onClick={async () => {
+                  invokeDeclareAnimal({
+                    args: [toFelt(sex), toFelt(legs), toFelt(wings)],
+                    overrides: { maxFee: 100000 },
+                  });
+                }}
+              >
+                Declare Animal
+              </button>
+            )}
+            <Group>{loading ? <div>loading...</div> : <div>{data}</div>}</Group>
+          </Group>
+        </>
       )}
-      <div>
-        <span>Name</span>
-        <span>{name !== '' && name}</span>
-      </div>
     </div>
   );
 };
